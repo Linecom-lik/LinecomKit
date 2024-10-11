2a025f50254b3348791923c1ac6da52e0262be86
Linecom-lik <3448336734@qq.com>
2024-10-11 20:26:26 +0800

    aa

--- LinecomKit_Previews/LinecomKit_Previews/ContentView.swift
+++ LinecomKit_Previews/LinecomKit_Previews/ContentView.swift
@@ -10,50 +10,23 @@ import SwiftUI
 import LinecomKit
 
 struct ContentView: View {
+    @State var checkstatus = false
     var body: some View {
-        NavigationStack {
-            TabView {
-                NStack {
-                    VStack {
-                        NeuButton(action: {
-                            AlertKitAPI.present(title: "Test", subtitle: "Test Message", icon: .done, style: .iOS17AppleMusic, haptic: .success)
-                        }, label: "Test Button")
+        List {
+            Button("Check JSONRequest") {
+                LinecomKit.NetworkAction.shared.requestJSON("https://api.linecom.net.cn/status/check") { resp succeed in
+                    if succeed {
+                        checkstatus = true
                     }
                 }
-                .navigationTitle("Buttons")
-                .tag(1)
-                .tabItem {
-                    Label("Button", systemImage: "square")
-                }
+            }
+            if checkstatus {
+                Text("successful!")
             }
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-        NStack {
-            NeuInput("Placeholder", icon: "globe", text: .constant(""))
-        }
-        NStack {
-            NeuButton(action: {}, label: "Test Button")
-        }
-        NStack {
-            NeuText("Neumorphic Text")
-        }
-        NStack {
-            NeuProgressView(value: 30, total: 100, foregroundColor: .blue, width: 300, height: 18)
-        }
-        NStack {
-            VStack {
-                NeuToggle("NToggle On", isOn: .constant(true), tint: .blue)
-                NeuToggle("NToggle Off", isOn: .constant(false))
-                NeuToggle(isOn: .constant(true), label: {
-                    Text("NToggle With View Label")
-                })
-            }
-        }
-        
-    }
+    ContentView()
 }
